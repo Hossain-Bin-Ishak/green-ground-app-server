049641a6e36d5de150b9f1e3fd08b6aebaf754b2
Hossain <web.developer.hossain@gmail.com>
2021-04-18 01:48:59 +0600

    First Commit

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# complete-website-server-web-developer-hossain
+# Green Ground - A Lawn Care Service Provider - Server Site App
+
+*** Version 1.0.0 ***
+
+#### App Description
+
+This a dynamic service site web application with some amazing and advanced features.
+
+You will find:
+
+- All type of services at a glace in the Home page.
+- Explore Option to place order details with dynamic features.
+
+Test the Live App: https://green-ground.web.app/
+# # Web Techonology Used
+
+- HtML5
+- CSS3
+- JavaScript
+- React
+- FontAwesome
+- Bootstarp
+- Node.js
+- Mongodb
+- Express.js
+
+# # Compatible Browsers	
+
+- IE10
+- IE11
+- Firefox
+- Safari
+- Opera
+- Chrome
+- Edge
+# # App Installation
+
+Use the package manager (https://github.com/Porgramming-Hero-web-course/complete-website-server-web-developer-hossain) to install Green Ground Web App.
+
+# # Contributor
+Hossain Bin Ishak <web.developer.hossain@gmail.com>
+
+# # License & Copyright
+© All Rights Reserved - Hossain Bin Ishak, Green Ground - A Lawn Care Service Provider Web App.
--- index.js
+++ index.js
@@ -28,6 +28,7 @@ client.connect(err => {
     const bookingsCollection = client.db("greenGround").collection("bookings");
     const servicesCollection = client.db("greenGround").collection("services");
     const reviewsCollection = client.db("greenGround").collection("reviews");
+    const adminsCollection = client.db("greenGround").collection("admins");
 
     console.log('Database connected successfully');
 
@@ -48,7 +49,8 @@ client.connect(err => {
     })
 
     app.get('/services/:id', (req, res) => {
-        bookingsCollection.find({ _id: ObjectId(req.params.id) })
+        console.log(req.params.id);
+        servicesCollection.find({ _id: ObjectId(req.params.id) })
             .toArray((err, document) => {
                 res.send(document[0]);
             })
@@ -73,17 +75,84 @@ client.connect(err => {
             })
     })
 
+    app.post('/addBooking', (req, res) => {
+        const bookings = req.body;
+        bookingsCollection.insertOne(bookings)
+            .then(result => {
 
-   
-    app.get('/bookings/:id', (req, res) => {
-        bookingsCollection.find({ _id: ObjectId(req.params.id) })
+                console.log(result);
+
+                res.send(result.insertedCount > 0)
+            })
+    })
+
+
+    app.post('/bookings', (req, res) => {
+        const date = req.body;
+        const email = req.body.email;
+        adminsCollection.find({ email: email })
+            .toArray((err, admins) => {
+                const filter = { date: date.date }
+                if (admins.length === 0) {
+                    filter.email = email;
+                }
+                bookingsCollection.find(filter)
+                    .toArray((err, documents) => {
+                        console.log(email, date.date, admins, documents)
+                        res.send(documents);
+                    })
+            })
+    })
+
+
+
+    app.get('/bookings', (req, res) => {
+        bookingsCollection.find()
+            .toArray((err, document) => {
+                res.send(document);
+                console.log(document);
+            })
+    })
+
+    app.post('/addAdmin', (req, res) => {
+        const admin = req.body;
+        adminsCollection.insertOne(admin)
+            .then(result => {
+
+                console.log(result);
+
+                res.send(result.insertedCount > 0)
+            })
+    })
+
+    app.get('/admins', (req, res) => {
+        adminsCollection.find()
+            .toArray((err, document) => {
+                res.send(document);
+                console.log(document);
+            })
+    })
+
+
+    app.post('/isAdmin', (req, res) => {
+        const email = req.body.email;
+        adminsCollection.find({ email: email })
+            .toArray((err, admins) => {
+                res.send(admins.length > 0);
+            })
+    })
+
+
+
+    app.get('/services/:id', (req, res) => {
+        servicesCollection.find({ _id: ObjectId(req.params.id) })
             .toArray((err, document) => {
                 res.send(document[0]);
             })
     })
 
     app.delete('/delete/:id', (req, res) => {
-        bookingsCollection.deleteOne({ _id: ObjectId(req.params.id) })
+        servicesCollection.deleteOne({ _id: ObjectId(req.params.id) })
             .then((result) => {
                 res.send(result.deletedCount > 0);
             })
